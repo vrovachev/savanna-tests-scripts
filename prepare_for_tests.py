157527578488e0ed0428dcc22eaeaf6bdfb67e88
Sergey Galkin <sgalkin@mirantis.com>
2013-12-23 18:05:37 +0400

    Inital fixes

--- prepare_for_tests.py
+++ prepare_for_tests.py
@@ -2,12 +2,18 @@
 import urllib
 import hashlib
 import os.path
-
+import logging
+import getpass
 import settings
 
 from glanceclient.v1 import Client as glanceclient
 from keystoneclient.v2_0 import Client as keystoneclient
 from novaclient.v1_1 import Client as novaclient
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+LOGGER.addHandler(ch)
 
 
 
@@ -16,7 +22,7 @@ class Common():
         self.controller_ip = controller_ip
 
     def _get_auth_url(self):
-        print('Slave-01 is {0}'.format(self.controller_ip))
+        LOGGER.debug('Slave-01 is {0}'.format(self.controller_ip))
         return 'http://{0}:5000/v2.0/'.format(self.controller_ip)
 
     def goodbye_security(self):
@@ -25,7 +31,7 @@ class Common():
                           api_key=settings.SERVTEST_PASSWORD,
                           project_id=settings.SERVTEST_TENANT,
                           auth_url=auth_url)
-        print('Permit all TCP and ICMP in security group default')
+        LOGGER.debug('Permit all TCP and ICMP in security group default')
         secgroup = nova.security_groups.find(name='default')
         nova.security_group_rules.create(secgroup.id,
                                          ip_protocol='tcp',
@@ -35,30 +41,52 @@ class Common():
                                          ip_protocol='icmp',
                                          from_port=-1,
                                          to_port=-1)
-        key_name = 'sgalkin'
+        key_name = getpass.getuser()
         if not nova.keypairs.findall(name=key_name):
-            print("Adding keys")
+            LOGGER.debug("Adding keys")
             with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
                 nova.keypairs.create(name=key_name, public_key=fpubkey.read())
-        if not flavor = nova.flavors.find(name='m1.small'):
-            nova.flavors.create('savanna',2048,1,40)
+        flavor = nova.flavors.find(name='m1.small')
+        if not flavor:
+            nova.flavors.create('savanna', 2048, 1, 40)
+
+    def check_image(self, url, image, md5,
+                    path=settings.SERVTEST_LOCAL_PATH):
+        download_url = "{0}/{1}".format(url, image)
+        local_path = "{0}/{1}".format(path, image)
+        LOGGER.debug('Check md5 {0} of image {1}/{2}'.format(md5, path, image))
+        if not os.path.isfile(local_path):
+            urllib.urlretrieve(download_url, local_path)
+        with open(local_path, mode='rb') as fimage:
+            digits = hashlib.md5()
+            while True:
+                buf = fimage.read(4096)
+                if not buf:
+                    break
+                digits.update(buf)
+            md5_local = digits.hexdigest()
+        if md5_local != md5:
+            LOGGER.debug('MD5 is not correct, download {0} to {1}'.format(
+                         download_url, local_path))
+            urllib.urlretrieve(download_url, local_path)
+        return True
 
     def image_import(self, properties, local_path, image, image_name):
-        print('Import image {0}/{1} to glance'.
+        LOGGER.debug('Import image {0}/{1} to glance'.
                      format(local_path, image))
         auth_url = self._get_auth_url()
-        print('Auth URL is {0}'.format(auth_url))
+        LOGGER.debug('Auth URL is {0}'.format(auth_url))
         keystone = keystoneclient(username=settings.SERVTEST_USERNAME,
                                   password=settings.SERVTEST_PASSWORD,
                                   tenant_name=settings.SERVTEST_TENANT,
                                   auth_url=auth_url)
         token = keystone.auth_token
-        print('Token is {0}'.format(token))
+        LOGGER.debug('Token is {0}'.format(token))
         glance_endpoint = keystone.service_catalog.url_for(
             service_type='image', endpoint_type='publicURL')
-        print('Glance endpoind is {0}'.format(glance_endpoint))
+        LOGGER.debug('Glance endpoind is {0}'.format(glance_endpoint))
         glance = glanceclient(endpoint=glance_endpoint, token=token)
-        print('Importing {0}'.format(image))
+        LOGGER.debug('Importing {0}'.format(image))
         with open('{0}/{1}'.format(local_path, image)) as fimage:
             glance.images.create(name=image_name, is_public=True,
                                  disk_format='qcow2',
@@ -66,28 +94,36 @@ class Common():
                                  properties=properties)
 
 
-common_func = Common('172.18.92.84')
-common_func.image_import(
-    settings.SERVTEST_SAVANNA_IMAGE_META,
-    settings.SERVTEST_LOCAL_PATH,
-    settings.SERVTEST_SAVANNA_IMAGE,
-    settings.SERVTEST_SAVANNA_IMAGE_NAME)
-common_func.image_import(
-    settings.SERVTEST_SAVANNA_IMAGE_META_2,
-    settings.SERVTEST_LOCAL_PATH,
-    settings.SERVTEST_SAVANNA_IMAGE_2,
-    settings.SERVTEST_SAVANNA_IMAGE_NAME_2)
-common_func.goodbye_security()
+if __name__ == "__main__":
+    import sys
+    controller = (sys.argv[1])
+    common_func = Common(controller)
+    common_func.image_import(
+        settings.SERVTEST_SAVANNA_IMAGE_META,
+        settings.SERVTEST_LOCAL_PATH,
+        settings.SERVTEST_SAVANNA_IMAGE,
+        settings.SERVTEST_SAVANNA_IMAGE_NAME)
+    common_func.image_import(
+        settings.SERVTEST_SAVANNA_IMAGE_META_2,
+        settings.SERVTEST_LOCAL_PATH,
+        settings.SERVTEST_SAVANNA_IMAGE_2,
+        settings.SERVTEST_SAVANNA_IMAGE_NAME_2)
+    common_func.goodbye_security()
 
-print('All done !')
-print ("HDP cluster:"
-       "Worker - ['TASKTRACKER', 'DATANODE', 'HDFS_CLIENT', 'MAPREDUCE_CLIENT']"
-       "Master - ['JOBTRACKER', 'NAMENODE', 'SECONDARY_NAMENODE', 'GANGLIA_SERVER', 'NAGIOS_SERVER' 'AMBARI_SERVER']")
-print ("sudo su hdfs"
-       "cd /usr/lib/hadoop"
-       "hadoop jar hadoop-examples.jar pi 10 10")
+LOGGER.debug('All done !')
+print("HDP cluster:\n"
+      "Creating:\n"
+      "Worker - ['TASKTRACKER', 'DATANODE', 'HDFS_CLIENT', "
+      "'MAPREDUCE_CLIENT']\n"
+      "Master - ['JOBTRACKER', 'NAMENODE', 'SECONDARY_NAMENODE',"
+      " 'GANGLIA_SERVER', 'NAGIOS_SERVER' 'AMBARI_SERVER']\n")
+print("Testing:\n"
+      "sudo su hdfs\n"
+      "cd /usr/lib/hadoop\n"
+      "hadoop jar hadoop-examples.jar pi 10 10\n")
 
-print("Vanilla cluster:"
-      "ubuntu@vvvv-v-master-001:~$ sudo su - hadoop"
-      "cd /usr/share/hadoop/"
-      "hadoop jar hadoop-examples-1.2.1.jar pi 10 10")
+print("Vanilla cluste r:\n"
+      "Testing:\n"
+      "sudo su - hadoop\n"
+      "cd /usr/share/hadoop/\n"
+      "hadoop jar hadoop-examples-1.2.1.jar pi 10 10\n")
