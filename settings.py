af691bf5d0dc7773aa053320cc305e1965bafa0f
Vadim Rovachev <vrovachev@mirantis.com>
2014-04-18 18:18:33 +0400

    minor change to settings

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ SERVTEST_TENANT = SERVTEST_USERNAME
 
 images = [
     {
-        "url": "http://sahara-files.mirantis.com/sahara-icehouse-vanilla-1.2.1-ubuntu-13.10.qcow2",
+        "url": "http://sahara-files.mirantis.com/",
         "image": "savanna-0.3-vanilla-1.2.1-ubuntu-13.04.qcow2",
         "name": "savanna1",
         "md5sum": "9ab37ec9a13bb005639331c4275a308d",
@@ -14,7 +14,7 @@ images = [
                  '_savanna_username': 'ubuntu'}
     },
     {
-        "url": "http://public-repo-1.hortonworks.com/sahara/images/centos-6_4-64-hdp-1.3.qcow2",
+        "url": "http://public-repo-1.hortonworks.com/sahara/images/",
         "image": "savanna-itests-ci-hdp-image-jdk-iptables-off.qcow2",
         "name": "hdp1",
         "md5sum": "4d90a480b6fc696ad38bfb4323a4a6ea",
@@ -22,7 +22,7 @@ images = [
                  '_savanna_username': 'root'}
     },
     {
-        "url": "http://sahara-files.mirantis.com/sahara-icehouse-vanilla-2.3.0-ubuntu-13.10.qcow2",
+        "url": "http://sahara-files.mirantis.com/",
         "image": "sahara-icehouse-vanilla-2.3.0-ubuntu-13.10.qcow2",
         "name": "savanna2",
         "md5sum": "",
@@ -30,7 +30,8 @@ images = [
                  '_savanna_username': 'ubuntu'}
     },
     {
-        "url": "https://s3.amazonaws.com/public-repo-1.hortonworks.com/sahara/images/centos-6_4-64-hdp-2_0_6.qcow2",
+        "url": "https://s3.amazonaws.com/public-repo-1.hortonworks."
+               "com/sahara/images/",
         "image": "centos-6_4-64-hdp-2_0_6.qcow2",
         "name": "hdp2",
         "md5sum": "",
